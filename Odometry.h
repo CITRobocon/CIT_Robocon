563d27fb9e7a9078b944eab1ff27ef14972fba10
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2018-08-02 20:40:23 +0900

    Update Odometry.h

--- Odometry.h
+++ Odometry.h
@@ -58,58 +58,58 @@
 #define PI 3.14159265358979
 
 class Odometry{
-  double ppr, gear_ratio, wheel_radius, length;
-  double period;
-  QEI *enc_r, *enc_l;
-  Ticker calculate;
+   double ppr, gear_ratio, wheel_radius, length;
+   double period;
+   QEI *enc_r, *enc_l;
+   Ticker calculate;
 
-  void calcOdometry (void){
-    double dr = -(double)(enc_r->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
-    double dl = (double)(enc_l->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
-    double dtheta = (dr-dl)/length;
+   void calcOdometry (void){
+      double dr = -(double)(enc_r->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
+      double dl = (double)(enc_l->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
+      double dtheta = (dr-dl)/length;
 
-    x += 0.50*(dr+dl)*cosf(ang+dtheta/2.0);
-    z += 0.50*(dr+dl)*sinf(ang+dtheta/2.0);
+      x += 0.50*(dr+dl)*cos(ang+dtheta/2.0);
+      z += 0.50*(dr+dl)*sin(ang+dtheta/2.0);
     
-    ang += dtheta;
-    if (ang < -PI)
-     ang += 2.0*PI;
-    else if (ang > PI)
-     ang -= 2.0*PI;
+      ang += dtheta;
+      if (ang < -PI)
+         ang += 2.0*PI;
+      else if (ang > PI)
+         ang -= 2.0*PI;
 
-    enc_r->reset();
-    enc_l->reset();
-  }
+      enc_r->reset();
+      enc_l->reset();
+   }
 
 public:
-  double x, z, ang;
+   double x, z, ang;
 
-  Odometry (QEI *qei1, QEI *qei2, double PPR, double ratio, double radius, double l, double T){
-    in.mode(PullUp);
-    enc_r = qei1;
-    enc_l = qei2;
-    gear_ratio = ratio;
-    wheel_radius = radius;
-    length = l;
-    ppr = PPR;
-    period = T;
-  }
+   Odometry (QEI *qei1, QEI *qei2, double PPR, double ratio, double radius, double l, double T){
+      in.mode(PullUp);
+      enc_r = qei1;
+      enc_l = qei2;
+      gear_ratio = ratio;
+      wheel_radius = radius;
+      length = l;
+      ppr = PPR;
+      period = T;
+   }
 
-  void setPosition (double x0, double z0, double ang0){
-    x = x0;
-    z = z0;
-    ang = ang0;
-  }
+   void setPosition (double x0, double z0, double ang0){
+      x = x0;
+      z = z0;
+      ang = ang0;
+   }
 
-  void start (void){
-    enc_r->reset();
-    enc_l->reset();
-    calculate.attach(this, &Odometry::calcOdometry, period);
-  }
+   void start (void){
+      enc_r->reset();
+      enc_l->reset();
+      calculate.attach(this, &Odometry::calcOdometry, period);
+   }
 
-  void stop (void){
-    calculate.detach();
-  }
+   void stop (void){
+       calculate.detach();
+   }
 };
 
 #endif //ODOMETRY_H
