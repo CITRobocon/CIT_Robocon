da01f5b42bf50f18eba1f6bba2390df2fcd6ff55
TomohiroAoki <a12tomo13@outlook.com>
2018-09-07 12:15:04 +0900

    Update AutoMachine.h
    戻した

--- AutoMachine.h
+++ AutoMachine.h
@@ -5,6 +5,13 @@
 #include "QEI.h"
 
 /*defines*/
+
+/*********************************machine**************************************/	
+#define WHEEL_DISTANCE 272.476	
+#define WHEEL_RADIUS 28	
+#define ENCODER_DISTANCE 405.6	
+#define ENCODER_RADIUS 25.5
+
 /*********************************LED******************************************/
 DigitalOut BLED1(PA_14);
 DigitalOut BLED2(PA_15);   
@@ -20,13 +27,17 @@ DigitalIn ASW(PC_10);
 
 /**********************************sensor**************************************/
 //PhotoReflector
+//     FL FR	
+//  L1       R1	
+//  L2       R2	
+//     BL BR
 AnalogIn FL(PC_2);
 AnalogIn FR(PC_3);
 AnalogIn L1(PC_0);
 AnalogIn L2(PC_1);
 AnalogIn BL(PB_0);
 AnalogIn BR(PA_4);
-AnalogIn R1(PA_1);
+AnalogIn R1(PA_1);//Unavailable
 AnalogIn R2(PA_0);
 
 //GatiasariOkiba
@@ -38,9 +49,9 @@ QEI ENC_L (PB_5, PB_4, PB_10, 2000);
 BusIn qei_in (PB_3, PA_2, PA_3, PB_5, PB_4, PB_10);
 
 /*********************************servo*****************************************/
-PwmOut servo1(PA_8);
-PwmOut servo2(PC_7);
-PwmOut servo3(PB_6);
+PwmOut servo1(PA_8);//sensor
+PwmOut servo2(PC_7);//elevate
+PwmOut servo3(PB_6);//table
 
 /********************************serial****************************************/
 Serial MD(PA_9,PA_10);
