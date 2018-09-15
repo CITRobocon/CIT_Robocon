c8c7e30fadb6a672f9806e622548f388a62a10a4
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2018-09-16 01:13:33 +0900

    Update Odometry.h

--- Odometry.h
+++ Odometry.h
@@ -1,82 +1,37 @@
-// Odometry.h
-// QEI.hを用いてオドメトリを取ります
-/*
-   クラス：
-     Odometry (class QEI *cp1, class QEI *cp2, double PPR, double rario, double radius, double l, double T)
-
-       class QEI *cp1 : 右側エンコーダのQEIクラスポインタ
-       class QEI *cp2 : 左側エンコーダのQEIクラスポインタ
-       double PPR : 一回転の総パルス数 (pulses per revolution)
-       double ratio : ギア比
-       double radius : 車輪半径
-       double l : 車輪間距離
-       double T : 計算周期 [s]
-
-    クラス内変数 (public)：
-      double x : オドメトリにより計算したx座標
-      double z : オドメトリにより計算したz座標
-      double ang : オドメトリにより計算した向いている方向 (-PI~PI) [rad]
-
-    クラス内関数 (public)：
-      void setPosition (double x0, double z0, double ang0)
-        入力した位置に設定します
-
-        double x0 : x座標
-        double z0 : z座標
-        double ang0 : 向いている角度
-
-      void start (void)
-        オドメトリの計算を開始します
-
-      void stop (void)
-        オドメトリの計算を中断します
-        
-   #sample#
-     QEI encR(GPIO1,GPIO2,NC,48,QEI::X4_ENCODING);
-     QEI encL(GPIO3,GPIO4,NC,48,QEI::X4_ENCODING);
-     BusIn in(GPIO1,GPIO2,GPIO3,GPIO4);
-
-     Odometry odometry(&encR, &encL, 48, 0.15, 0.025, 0.146, 0.020);
-
-     int main (void){
-        in.mode(PullUp);
-        odometry.setPositon(0.0, 0.0, 0.0);
-        odometry.start();
-        while(1)
-           printf("%lf, %lf, %lf\n\r", odometry.x, odometry.z, odometry.ang);
-     }
-   #sample#
-*/
-
 #ifndef ODOMETRY_H
 #define ODOMETRY_H
 
 #include "mbed.h"
 #include "QEI.h"
-#include "math.h"
 
-#define PI 3.14159265358979
+extern int dir;
 
 class Odometry{
    double ppr, gear_ratio, wheel_radius, length;
    double period;
    QEI *enc_r, *enc_l;
    Ticker calculate;
+   bool be_moving;
 
    void calcOdometry (void){
       double dr = -(double)(enc_r->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
       double dl = (double)(enc_l->getPulses())/ppr*gear_ratio*2.0*PI*wheel_radius;
       double dtheta = (dr-dl)/length;
 
+      if (dr == 0 && dl == 0)
+         be_moving = false;
+      else
+         be_moving = true;
+
       x += 0.50*(dr+dl)*cos(ang+dtheta/2.0);
       z += 0.50*(dr+dl)*sin(ang+dtheta/2.0);
-    
+
       ang += dtheta;
       if (ang < -PI)
          ang += 2.0*PI;
       else if (ang > PI)
          ang -= 2.0*PI;
-
+    
       enc_r->reset();
       enc_l->reset();
    }
@@ -84,7 +39,7 @@ class Odometry{
 public:
    double x, z, ang;
 
-   Odometry (QEI *qei1, QEI *qei2, double PPR, double ratio, double radius, double l, double T){
+   Odometry (QEI *qei1, QEI *qei2, double PPR, double ratio, double radius, double l, double T=0.010){
       enc_r = qei1;
       enc_l = qei2;
       gear_ratio = ratio;
@@ -108,6 +63,10 @@ public:
 
    void stop (void){
        calculate.detach();
+   }
+   
+   bool moving (void){
+      return  be_moving;
    }
 };
 
