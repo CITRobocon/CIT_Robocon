421b701faeff7982033a86c2e0c1c5009a0e68c1
sanoshohei <38240672+sanoshohei@users.noreply.github.com>
2018-09-16 01:12:43 +0900

    Control Functions of AutoMachine

--- ControlFunc.h
+++ ControlFunc.h
@@ -1,174 +1,246 @@
-// ControlFunc.h
-/*
-    class ControlFunc:
-        Public functions:
-            void followBrightLine (void)
-                フォトリフレクタを用いて白線を辿ります
-                
-            void followVirtualLine (double p[4][2], Odometry *odo)
-                オドメトリを用いて仮想の線を辿ります
-                
-                double p[4][2] : ベジェ曲線の制御点{{x0, y0}, ...{x3, y3}}
-                Odometry *odo : Odometryクラスポインタ
-                
-            void invert (void)
-                進行方向を反転させます
-                
-    #sample#
-        #include "ControlFunc.h"
-
-        int main (void){
-            Odometry odometry(&encR, &encL, 2000, 1.0, ENCODER_RADIUS, ENCODER_DISTANCE, 0.010);
-            ControlFunc controller;
-    
-            odometry.setPosition(0.0, 0.0, PI/2.0);
-            odometry.start();
-    
-            double p[4][2] = {{0.0, 0.0}, {0.0, 1000}, {1000, 1000}, {1000, 2000}};
-    
-            controller.followVirtualLine(p, &odometry);
-            controller.invert();
-            controller.followBrightLine();
-    
-            printf ("FINISH!\n\r");
-            while(1);
-        }
-    #sample#
-*/
+#ifndef CONTROLFUNC_H
+#define CONTROLFUNC_H
 
 #include "mbed.h"
-#include "SHIRAGIKU.h"
+#include "AutoMachine.h"
 #include "Odometry.h"
 #include "CubicCurve.h"
+#include "Sabertooth232.h"
 
-class ControlFunc {
-    double Kp_1, Ki_1, Kd_1, Kp_2, Ki_2, Kd_2;
-    double ep_1, ei_1, ed_1, ep_2, ei_2, ed_2, ep0_1, ep0_2;
-    double period;
-    float duty_1, duty_2;
-    double u;
-    int dir;
-    CubicCurve path;
-    Odometry *odometry;
-    Ticker control;
-    
-    double calcPID (void){
-        return Kp_1*ep_1 + Ki_1*ei_1 + Kd_1*ed_1 + Kp_2*ep_2 + Ki_2*ei_2 + Kd_2*Kd_2;
-    }
-    
-    void AdjustDutyRatio (void){
-        if (duty_1 > 1.0f){
-            duty_1 = 1.0f;
-            duty_2 = 0.0f;
-        }else if (duty_2 > 1.0f){
-            duty_1 = 0.0f;
-            duty_2 = 1.0f;
-        }
-        
-        duty_1 = duty_1*0.4f + 0.599f;
-        duty_2 = duty_2*0.4f + 0.599f;
-    }
-    
-    void control_PR (void){
-        if (dir == 1)
-            ep_1 = -PR_FR.read() + PR_FL.read();
-        else
-            ep_1 = PR_RR.read() - PR_RL.read();
-        ei_1 += ep_1*period;
-        ed_1 = (ep_1-ep0_1)/period;
-        ep0_1 = ep_1;
-        if (ei_1 > 1.0f)
-            ei_1 = 1.0f;
-        
-        duty_1 = 0.5f + calcPID();
-        duty_2 = 0.5f - calcPID();
-        
-        AdjustDutyRatio();
-        
-        m(dir*duty_2, dir*duty_1);
-    }
-    
-    void control_odometry (void){
-        double R = path.curvRad(u);
-        double ff;
-    
-        if (R == 0.0 || isnan(R))
-            ff = 0.0;
-        else
-            ff = 0.50*WHEEL_DISTANCE/2.0/R;
-        
-        ep_1 = dir*(-(path.x(u)-odometry->x)*sin(odometry->ang) + (path.y(u)-odometry->z)*cos(odometry->ang));
-        ei_1 += ep_1*period;
-        ed_1 = (ep_1-ep0_1)/period;
-        ep0_1 = ep_1;
-        if (ei_1 > 1000)
-            ei_1 = 1000;
-    
-        ep_2 = dir*(-odometry->ang + path.vecAng(u));
-        ei_2 += ep_2*period;
-        ed_2 = (ep_2-ep0_2)/period;
-        ep0_2 = ep_2;
-        if (ei_2 > 100)
-            ei_2 = 100;
-            
-        duty_1 = 0.5f + ff + calcPID();
-        duty_2 = 0.5f - ff - calcPID();
-        
-        AdjustDutyRatio();
-        
-        m(dir*duty_2, dir*duty_1);
-    
-        u = path.calcNearestVar(odometry->x,odometry->z,u);
-    }
-    
-public:
-    ControlFunc (void){
-        period = 0.020;
-        u = 0.0;
-        dir = 1;
-    }
+//for move
+float period = 0.050;
+float ave_duty, duty_1, duty_2, max_ratio = 1.0;
+char div_vel, stage_vel;
+float vel_grad[5];
+//odometry control
+double u = 0.0;
+float R;
+float ff;
+float fbGain_y, fbGain_theta;
+float cu_y, cu_theta;
+float dtheta;
+//state
+int dir = 1;
+int be_moving;
 
-    void followBrightLine (void){
-        Kp_1 = 1.0, Ki_1 = 0.010, Kd_1 = 0.00010;
-        Kp_2 = 0.0, Ki_2 = 0.0, Kd_2 = 0.0;
-        ei_1 = 0.0;
-        ep0_1 = 0.0;
+CubicCurve path;
+Ticker control_md;
+
+extern Odometry odometry;
+
+void AdjustDutyRatio (void){    
+    if (duty_1 > max_ratio)
+      duty_1 = max_ratio;
+    if (duty_1 < -max_ratio)
+      duty_1 = -max_ratio;
+      
+    if (duty_2 > max_ratio)
+      duty_2 = max_ratio;
+    if (duty_2 < -max_ratio)
+      duty_2 = -max_ratio;
+    
+    return;
+}
+
+void control_odometry_sfb (void){
+    u = path.calcNearestVar(odometry.x,odometry.z,u);
+    R = (float)path.curvRad(u);
+    
+    if (div_vel*u/(stage_vel+1) > 1.0)
+        stage_vel++;
         
-        control.attach(this, &ControlFunc::control_PR, period);
+    ave_duty = (1-div_vel*u+stage_vel)*vel_grad[stage_vel] + (div_vel*u-stage_vel)*vel_grad[stage_vel+1];
+    
+    if (R == 0.0f || isnan(R))
+        ff = 0.0f;
+    else
+        ff = ave_duty/R*(float)WHEEL_DISTANCE/2.0f;
         
-        if (dir == 1){
-            while (!PR_FRR);
-            while (PR_FRR);
-        }else{
-            while (!PR_RRR);
-            while (PR_RRR);
-        }
-        
-        control.detach();
-        m(2.0f, 2.0f);
+    if (ff > 3.0f*(1-ave_duty)/4.0f){
+      ff = 3.0f*(1.0f-ave_duty)/4.0f;
     }
     
-    void followVirtualLine (double p[4][2], Odometry *od){
-        Kp_1 = 0.025, Ki_1 = 0.00010, Kd_1 = 0.000010;
-        Kp_2 = 3.0, Ki_2 = 0.0030, Kd_2 = 0.00030;
-        ei_1 = 0.0;
-        ei_2 = 0.0;
-        ep0_1 = 0.0;
-        ep0_2 = 0.0;
-        u = 0.0;
-        
-        odometry = od;
-        path.bezier(p);
-        
-        control.attach(this, &ControlFunc::control_odometry, period);
-        
-        while(u < 1.0);
-        
-        control.detach();
-        m(2.0f,2.0f);
+    if (dir == 1){
+        cu_theta = (odometry.ang-path.vecAng(u));
+        cu_y = (-(odometry.x-path.x(u))*sin(path.vecAng(u)) + (odometry.z-path.y(u))*cos(path.vecAng(u)));
+    }else if (odometry.ang < 0.0){
+        cu_theta = (odometry.ang-path.vecAng(u)+PI);
+        cu_y = (-(odometry.x+150*cos(odometry.ang+PI)-path.x(u))*sin(path.vecAng(u)) + (odometry.z+150*sin(odometry.ang+PI)-path.y(u))*cos(path.vecAng(u)));
+    }else{
+        cu_theta = (odometry.ang-path.vecAng(u)-PI);
+        cu_y = (-(odometry.x+150*cos(odometry.ang-PI)-path.x(u))*sin(path.vecAng(u)) + (odometry.z+150*sin(odometry.ang-PI)-path.y(u))*cos(path.vecAng(u)));
     }
     
-    void invert (void){
-        dir *= -1;
+    if (cu_theta >= (float)PI)
+        cu_theta -= 2*PI;
+    else if (cu_theta <= -(float)PI)
+        cu_theta += 2*PI;
+    
+    dtheta = -fbGain_y/ave_duty/TOP_SPEED*cu_y - fbGain_theta*cu_theta;
+    
+    duty_1 = ave_duty + dir*(ff + dtheta*(float)WHEEL_DISTANCE/2.0f/TOP_SPEED);
+    duty_2 = ave_duty - dir*(ff + dtheta*(float)WHEEL_DISTANCE/2.0f/TOP_SPEED);
+    
+    AdjustDutyRatio();
+    writeMotor (dir*duty_1, dir*duty_2);
+    
+    if (u >= 1.0){
+        control_md.detach();
+        writeMotor(0.0f, 0.0f);
+        be_moving = 0;
     }
-};
+    
+    return;
+}
+
+//public-like
+
+void followVirtualLine (double p[4][2], float v0 = NAN, float v1 = NAN, float v2 = NAN, float v3 = NAN, float v4 = NAN){
+    while(be_moving){wait(0.010);};
+    if (isnan(v0)){
+        div_vel = 0;
+        vel_grad[0] = 0.5f;
+    }else if (isnan(v1)){
+        div_vel = 0;
+        vel_grad[0] = v0;
+    }else if (isnan(v2)){
+        div_vel = 1;
+        vel_grad[0] = v0;
+        vel_grad[1] = v1;
+    }else if (isnan(v3)){
+        div_vel = 2;
+        vel_grad[0] = v0;
+        vel_grad[1] = v1;
+        vel_grad[2] = v2;
+    }else if (isnan(v4)){
+        div_vel = 3;
+        vel_grad[0] = v0;
+        vel_grad[1] = v1;
+        vel_grad[2] = v2;
+        vel_grad[3] = v3;
+    }else{
+        div_vel = 4;
+        vel_grad[0] = v0;
+        vel_grad[1] = v1;
+        vel_grad[2] = v2;
+        vel_grad[3] = v3;
+        vel_grad[4] = v4;
+    }
+    stage_vel = 0;
+    
+    u = 0.0;
+    path.bezier(p);
+    
+    if (dir == 1)
+        fbGain_y = 10, fbGain_theta = 7;
+    else
+        fbGain_y = 15, fbGain_theta = 5;
+    
+    control_md.attach(&control_odometry_sfb, period);
+    be_moving = 1;
+    
+    return;
+}
+
+void invert (void){
+    dir *= -1;
+    return;
+}
+
+void setMD_maxRatio (float ratio){
+    max_ratio = ratio;
+    return;
+}
+
+int moving (void){
+    if (be_moving)
+        return 1;
+    else
+        return 0;
+}
+
+void forciblyStop (void){
+    control_md.detach();
+    be_moving = 0;
+    return;
+}
+
+void turnEast (void){
+    be_moving = 1;
+    writeMotor(-0.6f, 0.6f);
+    wait(0.3);
+    while (odometry.ang < 0.0){wait(0.010);}
+    while (odometry.ang > 0.0){wait(0.010);}
+    writeMotor(0.0f, 0.0f);
+    be_moving = 0;
+    return;
+}
+
+void turnWest (void){
+    be_moving = 1;
+    writeMotor(0.6f, -0.6f);
+    wait(0.3);
+    while (odometry.ang < 0.0){wait(0.010);}
+    while (odometry.ang > 0.0){wait(0.010);}
+    writeMotor(0.0f, 0.0f);
+    be_moving = 0;
+    return;
+}
+
+
+/*for table*/
+int onGA (void){
+  return (int)(GA.read()*5);
+}
+
+void putGA (void){
+    servo3.pulsewidth_us(720);
+    return;
+}
+
+void openTable (void){
+    servo3.pulsewidth_us(920);
+    return;    
+}
+
+void closeTable (void){
+    servo3.pulsewidth_us(1950);
+    return;        
+}
+
+
+/*for hand*/
+void grabA (void){
+    servo1.pulsewidth_us(2310);
+    return;
+}
+
+void releaseA (void){
+    servo1.pulsewidth_us(2120);
+    return;
+}
+
+void lowerHandToReleaseA (void){
+    servo2.pulsewidth_us(1050);
+    return; 
+}
+
+void lowerHandToGrabA (void){
+    servo2.pulsewidth_us(1620);
+    return; 
+}
+
+void raiseHand (void){
+    servo2.pulsewidth_us(700);
+    return;
+}
+
+
+/*for etc*/
+void waitPushASW (void){
+    while(!(ASW&1));
+    while(ASW&1);
+    return;
+}
+
+#endif //CONTROLFUNC_H
